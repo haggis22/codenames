2abb5e0b431e25e83354c1252763b246d813aa3d
haggis22 <dshell@gmail.com>
2017-01-14 23:57:22 -0600

    added gulp file for concat task instead of browserify

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp');
+var del = require('del');
+var concat = require('gulp-concat');
+
+
+gulp.task('client-js', function() {
+
+    return gulp.src([ 'src/app/app.js', 'src/modules/**/*.js' ])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('client/js'));
+
+});
+
+gulp.task('default', ['client-js']);
