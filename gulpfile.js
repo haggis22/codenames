88fe11cea5f1d0eb1bd572e9e75324fb170c5a9e
haggis22 <dshell@gmail.com>
2017-01-16 17:57:03 -0600

    introduced sass preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 ﻿var gulp = require('gulp');
 var del = require('del');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 
+
+gulp.task('css', function () {
+
+    return gulp.src('src/css/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('client/css'));
+
+
+});
 
 gulp.task('client-js', function() {
 
