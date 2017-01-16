494f3aeccc23c05ca28aae50ca79a03718b54218
haggis22 <dshell@gmail.com>
2017-01-16 17:57:30 -0600

    added server-side javascrit objects

---
+++ js/users/Session.js
@@ -0,0 +1,30 @@
+﻿(function (isNode, isAngular) {
+
+    "use strict";
+
+    var SessionModule = function () {
+
+        var Session = function (session) {
+
+            this.hash = session.hash;
+            this.username = session.username;
+
+        };
+
+        return Session;
+
+    };
+
+    if (isAngular) {
+        // AngularJS module definition
+        angular.module('codenames.app')
+            .factory('Session', [SessionModule]);
+
+    }
+    else if (isNode) {
+        // NodeJS module definition
+        module.exports = SessionModule();
+
+    }
+
+})(typeof module !== 'undefined' && module.exports, typeof angular !== 'undefined');
---
+++ js/users/User.js
@@ -0,0 +1,33 @@
+﻿(function (isNode, isAngular) {
+
+    "use strict";
+
+    var UserModule = function () {
+
+        var User = function (user) {
+
+            this._id = user._id;
+            this.username = user.username;
+            this.password = user.password;
+            this.updated = user.updated;
+            this.sessionHash = user.sessionHash;
+
+        };
+
+        return User;
+
+    };
+
+    if (isAngular) {
+        // AngularJS module definition
+        angular.module('codenames.app')
+            .factory('User', [UserModule]);
+
+    }
+    else if (isNode) {
+        // NodeJS module definition
+        module.exports = UserModule();
+
+    }
+
+})(typeof module !== 'undefined' && module.exports, typeof angular !== 'undefined');
