45be586f85ecf7b21f4c38184f998e3160ab3580
haggis22 <dshell@gmail.com>
2017-01-28 20:15:37 -0600

    added directives for focus-me and enter-action

---
+++ src/directives/enter-action.js
@@ -0,0 +1,22 @@
+﻿(function (app) {
+
+    "use strict";
+
+    app.directive('enterAction', [ function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.enterAction);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+        
+    }]);
+
+})(angular.module('codenames.app'));
+
+
