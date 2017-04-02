c0efc34db0866f8e32a512c8417022190936c711
haggis22 <dshell@gmail.com>
2017-04-01 22:23:35 -0500

    now that mongodb is local, adding prod config to git

---
+++ configs/prod/config.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+var config = {};
+
+config.servers =
+[
+    { secure: false, port: 8080, ip: '0.0.0.0' },
+    { secure: true, port: 4343, ip: '0.0.0.0', keyFile: './server/certs/haggis22.com.key', certFile: './server/certs/haggis22.com.crt' }
+];
+
+
+config.db =
+{
+    users: "localhost:27017/users",
+    codenames: "localhost:27017/codenames"
+};
+
+config.logging =
+{
+    configFile: __dirname + "/log4js_config.json",
+    main: "codenames"
+};
+
+
+module.exports = config;
